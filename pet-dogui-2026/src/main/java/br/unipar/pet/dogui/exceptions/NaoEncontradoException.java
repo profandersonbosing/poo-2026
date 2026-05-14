5ae2a9478f083e4af756cdc3315b0b9573387269
profandersonbosing <110434492+profandersonbosing@users.noreply.github.com>
2026-05-14 20:17:24 -0300

    Add files via upload

---
+++ pet-dogui-2026/src/main/java/br/unipar/pet/dogui/exceptions/NaoEncontradoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.pet.dogui.exceptions;
+
+/**
+ *
+ * @author andersonbosing
+ */
+public class NaoEncontradoException extends Exception {
+
+    public NaoEncontradoException(String message) {
+        super(message);
+    }
+    
+}
--- pet-dogui-2026/src/main/java/br/unipar/pet/dogui/services/EspecieService.java
+++ pet-dogui-2026/src/main/java/br/unipar/pet/dogui/services/EspecieService.java
@@ -5,9 +5,11 @@
 package br.unipar.pet.dogui.services;
 
 import br.unipar.pet.dogui.domains.Especie;
+import br.unipar.pet.dogui.exceptions.NaoEncontradoException;
 import br.unipar.pet.dogui.exceptions.ValidacaoNegocioException;
 import br.unipar.pet.dogui.repositories.EspecieRepository;
 import br.unipar.pet.dogui.repositories.interfaces.EspecieRepositoryInterface;
+import java.util.ArrayList;
 
 /**
  *
@@ -50,9 +52,65 @@ public class EspecieService {
         return especie;
     }
     
-    //atualizar
-    //deletar
-    //findById
-    //listarTodos
-    
+    public Especie atualizar(Especie especie) throws ValidacaoNegocioException, NaoEncontradoException {
+
+        if (especie == null) {
+            throw new ValidacaoNegocioException("Obrigatório informar a Especie para atualização");
+        }
+        
+        if (especie.getId() == null) {
+            throw new ValidacaoNegocioException("Obrigatório informar o ID da Especie para atualização");
+        }
+       
+        Especie especieValidacao = especieRepositoryInterface.findById(especie.getId());
+
+        if (especie == null) {
+            throw new NaoEncontradoException("Especie não encontrada para o ID informado");
+        }
+
+        if (especie.getNome() == null || especie.getNome().isBlank()) {
+            throw new ValidacaoNegocioException("Informe o Nome da Especie");
+        }
+
+        if (especie.getNome().length() < 2) {
+            throw new ValidacaoNegocioException("O nome da Especie precisa conter dois ou mais caracteres");
+        }
+
+        if (especie.getNome().length() > 60) {
+            throw new ValidacaoNegocioException("O nome da especie deve conter menos que 60 caracteres");
+        }
+
+        especie = especieRepositoryInterface.atualizar(especie);
+
+        return especie;
+    }
+
+    public void deletar(Long id) throws ValidacaoNegocioException {
+
+        if (id == null) {
+            throw new ValidacaoNegocioException("Obrigatório informar o ID da Especie para exclusão");
+        }
+
+        especieRepositoryInterface.deletar(id);
+    }
+
+    public Especie findById(Long id) throws ValidacaoNegocioException, NaoEncontradoException {
+
+        if (id == null) {
+            throw new ValidacaoNegocioException("Obrigatório informar o ID da Especie para busca");
+        }
+
+        Especie especie = especieRepositoryInterface.findById(id);
+
+        if (especie == null) {
+            throw new NaoEncontradoException("Especie não encontrada para o ID informado");
+        }
+
+        return especie;
+    }
+
+    public ArrayList<Especie> listarTodos() {
+        return especieRepositoryInterface.listarTodos();
+    }
+
 }
